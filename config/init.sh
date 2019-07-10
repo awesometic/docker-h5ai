4d1201c82fb9cdc4db02216c3ff303c91265a1bb
Yang Deokgyu <secugyu@gmail.com>
2019-07-10 16:59:32 +0900

    Fix to apply new h5ai version when there is previous one
    Signed-off-by: Yang Deokgyu <secugyu@gmail.com>

--- config/init.sh
+++ config/init.sh
@@ -24,6 +24,7 @@ orig_h5ai="/usr/share/h5ai/_h5ai"
 conf_nginx="/config/nginx/h5ai.conf"
 conf_h5ai="/config/h5ai/_h5ai"
 conf_htpwd="/config/nginx/.htpasswd"
+options_file="/private/conf/options.json"
 
 msg "Check configuration files for Nginx..."
 if [ ! -f "$conf_nginx" ]; then
@@ -42,7 +43,22 @@ if [ ! -d "$conf_h5ai" ]; then
     cp -arf $orig_h5ai $conf_h5ai
 else
     msg "User setup files found: $conf_h5ai"
-    msg "Remove image's default setup files and copy the previous version..."
+
+    msg "Check if h5ai version updated..."
+    new_ver=$(head -n 1 $orig_h5ai$options_file | awk '{print $3}' | sed 's/[^0-9]//g')
+    pre_ver=$(head -n 1 $conf_h5ai$options_file | awk '{print $3}' | sed 's/[^0-9]//g')
+    if [ $new_ver -gt $pre_ver ]; then
+		msg "New version detected. Make existing options.json backup file..."
+		cp $conf_h5ai$options_file /config/$(date '+%Y%m%d_%H%M%S')_options.json.bak
+
+		msg "Remove existing h5ai files..."
+		rm -rf $conf_h5ai
+
+		msg "Copy the new version..."
+		cp -arf $orig_h5ai $conf_h5ai
+	fi
+
+    msg "Remove image's default setup files and copy the existing version..."
 fi
 rm -rf $orig_h5ai
 ln -s $conf_h5ai $orig_h5ai
